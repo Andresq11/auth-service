c6e283bef75e0d62ae0c50205b08fbb3ed188542
Andresq11 <andresbossio2005@gmail.com>
2026-05-06 19:51:23 -0500

    Repositorio Auth-service

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,10 +10,14 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        // usuario real para login
         User::create([
             'name'     => 'Admin',
             'email'    => 'admin@test.com',
             'password' => Hash::make('password123'),
         ]);
+
+        // usuarios de prueba
+        User::factory(5)->create();
     }
 }
